47cf191a58c230b973d0979de018d74cf21013b0
mkulumadzi <evan.waters@gmail.com>
2014-12-16 12:47:34 -0500

    Added rendering and options parsing

--- lib/mediawiki-hana/application.rb
+++ lib/mediawiki-hana/application.rb
@@ -1,3 +1,98 @@
-def hello
-	"Hello world!"
+require 'optparse'
+
+class Application
+
+	attr_reader :wiki_query, :search_string, :params
+
+	include MediaWiki
+
+	def parse_options(argv)
+		@params = {}
+		opts = OptionParser.new
+		opts.banner = "Usage: QUERY [options]"
+		opts.separator ""
+		opts.separator "Specific options:"
+
+		opts.on("--text", "Render to a text file") { @params[:render_mode] = :text }
+		opts.on("--terminal", "Render to the terminal") { @params[:render_mode] = :terminal }
+		opts.on("--json", "Render to a json file") { @params[:render_mode] = :json }
+
+		opts.on("-o", "--output [FILENAME]", "Output to a file") { |f| @params[:output_file] = f }
+
+		opts.on_tail("-h", "--help", "Show this message") do
+			puts opts
+			exit
+		end
+		# parser.on('-o') do
+		# 	parser.on('data/output.txt') { @params[:output_file] = 'data/output.txt' }
+		# end
+
+		@search_string = opts.parse(argv)[0]
+
+	end
+
+	def initialize(argv)
+		parse_options(argv)
+		@wiki_query = MediaWiki::Query.new(@search_string)
+	end
+
+	# For each page, print the search string, title andshort summary
+	def render
+
+		case @params[:render_mode]
+		when :terminal
+			render_to_terminal
+		when :text
+			render_to_text
+		when :json
+			render_to_json
+		else
+			raise ArgumentError, "Invalid render mode"
+		end	
+
+	end
+
+	def render_to_terminal
+
+		@wiki_query.pages.each do |key, page|
+			puts "Search string: #{key}"
+			puts "Page title returned: #{page.title}" 
+			puts "Summary: #{page.summary}\n\n"
+		end
+
+		return nil
+
+	end
+
+	def render_to_text
+
+		f = File.new(@params[:output_file], 'w')
+
+		@wiki_query.pages.each do |key, page|
+			f << "Search string: #{key}\n"
+			f << "Page title returned: #{page.title}\n" 
+			f << "Summary: #{page.summary}\n\n"
+		end
+
+		f.close
+
+	end
+
+	def render_to_json
+		f = File.new(@params[:output_file], 'w')
+		f << @wiki_query.query_result
+		f.close
+	end
+
 end
+
+# Placeholder pseudo code
+
+## Enter one more more search strings
+## Get the pages back, store them
+
+## Render the pages in a few different ways
+## Save a .csv file with the search string, title and summary
+## Print the search string, title and summary to the command line
+## Save a .json file with the full query content
+## Save a .txt file with the full text content, one page at a time
--- spec/lib/mediawiki-hana/application_spec.rb
+++ spec/lib/mediawiki-hana/application_spec.rb
@@ -1,9 +1,182 @@
 require_relative '../../spec_helper'
 
-describe "hello world" do
+describe Application do
 
-	it "must say hello world" do
-		hello.must_equal "Hello world!"
+	describe "default attributes" do
+
+		it "must include the Mediawiki methods" do
+			Application.must_include MediaWiki
+		end
+
+	end
+
+	describe "parse options" do
+
+		it "must store a hash of the parameters" do
+			application = Application.new(['Main Page', '--text'])
+			application.params.must_be_instance_of Hash
+		end
+
+		describe "render mode" do
+
+			it "must set the mode to :text if the --text option is given" do
+				application = Application.new(['Main Page', '--text'])
+				application.params[:render_mode].must_equal :text
+			end
+
+			it "must set the mode to :terminal if the --terminal option is given" do
+				application = Application.new(['Main Page', '--terminal'])
+				application.params[:render_mode].must_equal :terminal
+			end
+
+			it "must set the mode to json if the --json option is given" do
+				application = Application.new(['Main Page', '--json'])
+				application.params[:render_mode].must_equal :json
+			end
+
+			it "must set the output file if the -o option is given" do
+				application = Application.new(['Main Page', '--text', '-o', 'data/output.txt'])
+				application.params[:output_file].must_equal 'data/output.txt'
+			end
+
+			it "must set the output file if the --output option is given" do
+				application = Application.new(['Main Page', '--text', '--output', 'data/output.txt'])
+				application.params[:output_file].must_equal 'data/output.txt'
+			end
+
+		end
+
+		describe "show help text" do
+
+			before do
+				$stdout = StringIO.new
+			end
+
+			it "must show the help text if the --help option is given" do
+				begin
+					Application.new(['--help'])
+				rescue SystemExit
+				end
+				$stdout.string.split("\n")[0].must_equal "Usage: QUERY [options]"
+			end
+
+			it "must show the help text if the --h option is given" do
+				begin
+					Application.new(['--h'])
+				rescue SystemExit
+				end
+				$stdout.string.split("\n")[0].must_equal "Usage: QUERY [options]"
+			end
+
+		end
+
+		it "must store the remaining parameters as the search string" do
+			application = Application.new(['Main Page', '--terminal'])
+			application.search_string.must_equal "Main Page"
+		end
+
+	end
+
+	describe "get MediaQuery" do
+
+		let(:application) {Application.new(['Main Page', '--terminal'])}
+
+		it "must get a valid MediaWiki query object" do
+			application.wiki_query.must_be_instance_of MediaWiki::Query
+		end
+
+		it "must get the right search string do" do
+			application.wiki_query.query.must_equal "Main Page"
+		end
+
+	end
+
+	describe "rendering a single page" do
+
+		describe "render to the terminal" do
+
+			let(:application) { Application.new(['Main Page', '--terminal'])}
+
+			before do
+				$stdout = StringIO.new
+				application.render
+				@result = $stdout.string.split("\n")
+			end
+
+			it "must have a render method" do
+				application.must_respond_to :render
+			end
+
+			it "must print the search string on the first line" do
+				@result[0].must_equal "Search string: Main Page"
+			end
+
+			it "must print the page title on the second line" do
+				@result[1].must_equal "Page title returned: Main Page"
+			end
+
+			# Summary has a few extra new lines
+			it "must print the page summary" do
+				@result[4].must_equal "Welcome to Wikipedia,"
+			end
+
+		end
+
+		describe "render to a text file" do
+
+			let(:application) { Application.new(['Main Page', '--text', '-o', 'data/output.txt']) }
+
+			before do
+				application.render
+				@result = []
+
+				File.open('data/output.txt', 'r') do |f|
+					f.each_line do |line|
+						@result << line.chomp
+					end
+				end
+
+			end
+
+			after do
+				File.delete('data/output.txt')
+			end
+
+			it "must print the search string on the first line" do
+				@result[0].must_equal "Search string: Main Page"
+			end
+
+			it "must print the page title on the second line" do
+				@result[1].must_equal "Page title returned: Main Page"
+			end
+
+			# Summary has a few extra new lines
+			it "must print the page summary" do
+				@result[4].must_equal "Welcome to Wikipedia,"
+			end
+			
+		end
+
+		describe "render to a json file" do
+
+			let(:application) { Application.new(['Main Page', '--json', '-o', 'data/output.json']) }
+
+			before do
+				application.render
+				File.open('data/output.json', 'r') { |f| @result = f.read }
+			end
+
+			after do
+				File.delete('data/output.json')
+			end
+
+			# To Do: Do a better job of testing for JSON...
+			it "must save the query result to the file" do
+				@result.index("{").must_be_instance_of Fixnum
+			end
+
+		end
+
 	end
 
 end
